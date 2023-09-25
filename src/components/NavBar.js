424af800b4069b695cb08f6c8b74c6f35577b07a
satyajeet <satyajeetrajupali@gmail.com>
2023-09-25 15:42:31 +0530

    Added logo and icon for the application

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = (props) => {
     <div>
       <nav className={`navbar fixed-top navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/"><strong>NewsAgent</strong></Link>
+          <Link className="navbar-brand" to="/"><strong>NewsAgent 📰</strong></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
